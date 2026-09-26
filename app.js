0acab1071994b7b512607c545c239fb65a3e6053
alexiskrazy1 <gcancelliere04@gmail.com>
2026-09-26 01:43:03 -0400

    Added simulation login JSON response

--- app.js
+++ app.js
@@ -940,26 +940,11 @@ if (loginForm) {
             try {
 
                 const response =
-                    await fetch(
-                        "/api/login",
-                        {
-                            method: "POST",
-
-                            headers: {
-                                "Content-Type":
-                                    "application/json"
-                            },
-
-                            body: JSON.stringify({
-                                email: email,
-                                password: password
-                            })
-                        }
-                    );
-
+                     await fetch("login-data.json");
 
                 const data =
                     await response.json();
+
 
 
                 const message =
